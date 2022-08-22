c1934d4169ff558d9fb8aec10e8b425763693a09
progwruk <wmeshram@gmail.com>
2022-08-22 18:25:15 +0530

    [Wrukshali] Add: Attendence

---
+++ EmployeeWageComputation/EmployeeWage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class EmployeeWage
+    {
+        public void Attendence()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2); //0,1
+            if (empCheck == 0)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+        }
+    }
+}
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeWage a = new EmployeeWage();
+            a.Attendence();
+    
+        }
+    }
+}
